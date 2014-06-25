bf08a3b71ca74667ef0d5608e23f52d43c74d79a
Timm <timm930@gmail.com>
2014-06-26 00:23:57 +0800

    change file name

--- TocHW4.java
+++ TocHW4.java
@@ -17,7 +17,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.lang.String;
 
-public class TocHW4 {
+public class TocHw4 {
 	public static class Road{
 		private String roadName;
 		private int maxPrice;
